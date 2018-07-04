c8cc111d9d3122c0a00c9f5c9f3b671a7826a706
Otabek Kamalov <otakam86@gmail.com>
2018-07-04 14:48:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var name = "Joe"
+/*var name = "Joe"
 var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
+*/
+var combos = function (arr) {
+  var combinations = [];
+  for (var i = 0; i<arr.length-1;i++){
+    for (var j=i+1;j<arr.length;j++){
+      combinations.push([arr[i],arr[j]]);
+    }
+  }
+  console.log(combinations)
+}
